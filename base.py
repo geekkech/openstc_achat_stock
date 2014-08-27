b2338a6bc3a4ef684c1ad3d6fd79dde3d5a397ec
bplancher <bruno.plancher@atreal.fr>
2014-08-27 14:52:04 +0200

    [ir.attachment] added fields to manage 'to_pay' and 'is_paid' mail sending; implemented 'actions' field; override write() method to evolve state according to fields values; mail failures are stored in selection fields instead of 'state' field

--- base.py
+++ base.py
@@ -73,20 +73,62 @@ class ir_attachment(OpenbaseCore):
     _name = "ir.attachment"
     _columns = {
         'state':fields.selection([('to_check','A Traiter'),('validated','Facture Validée'),
-                                  ('refused','Facture Refusée'),('not_invoice','RAS'),('except_send_mail','Echec envoi du mail')], 'Etat', readonly=True),
+                                  ('refused','Facture Refusée'),('not_invoice','RAS'),('except_send_mail','Echec envoi du mail'), (('except_send_mail_is_paid','Echec envoi du mail'))], 'Etat', readonly=True),
          'action_date':fields.datetime('Date de la derniere action', readonly=True),
          'engage_done':fields.boolean('Suivi commande Clos',readonly=True),
          'attach_made_done':fields.boolean('Cette Facture Clos cette commande', readonly=True),
+         
+         'to_pay':fields.selection([('yes', 'Yes'), ('no', 'No')], 'To pay ?'),
          'justif_refuse':fields.text('Justificatif Refus', state={'required':[('state','=','refused')], 'invisible':[('state','!=','refused')]}),
+         'send_mail_to_pay':fields.boolean('Send mail "TO PAY" ?'),
+         'mail_to_pay_status':fields.selection([('sent', 'Sent'), ('failure', 'Failure')], 'mail "TOPAY" status'),
+         
+         'is_paid':fields.selection([('yes', 'Yes'), ('no', 'No')], 'Is it paid ?'),
+         'justif_refuse_payment':fields.text('Justificatif Refus', state={'required':[('state','=','refused')], 'invisible':[('state','!=','refused')]}),
+         'send_mail_is_paid':fields.boolean('Send mail "IS PAID" ?'),
+         'mail_is_paid_status':fields.selection([('sent', 'Sent'), ('failure', 'Failure')], 'mail "ISPAID" status'),
+         
          'is_invoice': fields.boolean('Is PDF invoice'),
         }
     
+    _actions = {
+        'confirm': lambda self,cr,uid,record,groups_code: record.is_invoice and record.state in ['to_check', 'except_send_mail'],
+        'pay': lambda self,cr,uid,record,groups_code: record.is_invoice and record.state in ['validated', 'except_send_mail', 'except_send_mail_is_paid'],
+        }
+    
     _defaults = {
+        'send_mail_to_pay': lambda *a: False,
+        'send_mail_is_paid': lambda *a: False,
         'state':'not_invoice',
         'is_invoice': lambda *a: False,
         }
     
     _order = "create_date"
+    
+    def apply_state_changes(self, cr, uid, ids, context=None):
+        for attach in self.browse(cr, uid, ids, context=context):
+            #check here too if attach is invoice, because this method is available too throw xmlrpc call, without passing to write()
+            actions = attach.actions
+            if attach.is_invoice:
+                if 'confirm' in actions or attach.send_mail_to_pay:
+                    if attach.to_pay == 'yes':
+                        attach.send_invoice_to_pay(context=context)
+                    elif attach.to_pay == 'no':
+                        attach.refuse_invoice_to_pay(context=context)
+                elif 'pay' in actions or attach.send_mail_is_paid:
+                    if attach.is_paid == 'yes':
+                        pass
+                    elif attach.to_pay == 'no':
+                        pass
+        return
+    
+    ## Override to evolve state of invoice-attachment
+    def write(self, cr, uid, ids, vals, context=None):
+        ret = super(ir_attachment, self).write(cr, uid, ids, vals, context=context)
+        #retrieve attachment-invoices only and perform state changes
+        ret_ids = self.search(cr, uid, [('id', 'in', ids), ('is_invoice', '=', True)], context=context)
+        self.apply_state_changes(cr, uid, ret_ids, context=context)
+        return ret
     
     #Override to put an attach as a pdf invoice if it responds to the pattern 
     def create(self, cr, uid, vals, context=None):
@@ -112,16 +154,23 @@ class ir_attachment(OpenbaseCore):
         if isinstance(ids, list):
             ids = ids[0]
         attach = self.browse(cr, uid, ids, context)
-        template_id = self.pool.get("email.template").search(cr, uid, [('model_id','=','purchase.order'),('name','like','%Valid%')], context=context)
-        if isinstance(template_id, list):
-            template_id = template_id[0]
-        msg_id = self.pool.get("email.template").send_mail(cr, uid, template_id, attach.res_id, force_send=False, context=context)
-        self.pool.get("mail.message").write(cr, uid, [msg_id], {'attachment_ids':[(4, ids)]}, context=context)
-        self.pool.get("mail.message").send(cr, uid, [msg_id], context)
-        if self.pool.get("mail.message").read(cr, uid, msg_id, ['state'], context)['state'] == 'exception':
-            self.write(cr, uid, [ids], {'state':'except_send_mail','action_date':datetime.now()}, context)
-        else:
-            self.write(cr, uid, [ids], {'state':'validated','action_date':datetime.now()}, context)
+        mail_status = False
+        state = 'validated'
+        if attach.send_mail_to_pay:
+            mail_status = 'sent'
+            template_id = self.pool.get("email.template").search(cr, uid, [('model_id','=','purchase.order'),('name','like','%Valid%')], context=context)
+            if isinstance(template_id, list):
+                template_id = template_id[0]
+            msg_id = self.pool.get("email.template").send_mail(cr, uid, template_id, attach.res_id, force_send=False, context=context)
+            self.pool.get("mail.message").write(cr, uid, [msg_id], {'attachment_ids':[(4, ids)]}, context=context)
+            self.pool.get("mail.message").send(cr, uid, [msg_id], context)
+            if self.pool.get("mail.message").read(cr, uid, msg_id, ['state'], context)['state'] == 'exception':
+                mail_status = 'failure'
+                #state = 'except_send_mail'
+        vals = {'send_mail_to_pay': False, 'state':state, 'action_date':datetime.now()}
+        if mail_status:
+            vals.update({'mail_to_pay_status': mail_status})
+        self.write(cr, uid, [ids], vals, context)
         
         return {'res_model':'purchase.order',
                 'view_mode':'form,tree',
@@ -140,29 +189,38 @@ class ir_attachment(OpenbaseCore):
                 'target':'new',
                 'context':{'attach_id':ids}
                 }
-    
+    #@TODO: Refactor this, to use the same code as send_invoice_to_pay, but with new parameter: 'template_id' 
     def refuse_invoice_to_pay(self, cr, uid, ids, context=None):
         #ids refer to attachment that need to be sent with mail
         #Envoie de la piece jointe en mail au service compta
         if isinstance(ids, list):
             ids = ids[0]
         attach = self.browse(cr, uid, ids, context)
-        template_id = self.pool.get("email.template").search(cr, uid, [('model_id','=','purchase.order'),('name','like','%Refus%')], context=context)
-        if isinstance(template_id, list):
-            template_id = template_id[0]
-        msg_id = self.pool.get("email.template").send_mail(cr, uid, template_id, attach.res_id, force_send=False, context=context)
-        self.pool.get("mail.message").write(cr, uid, [msg_id], {'attachment_ids':[(4, ids)]}, context=context)
-        self.pool.get("mail.message").send(cr, uid, [msg_id], context)
-        if self.pool.get("mail.message").read(cr, uid, msg_id, ['state'], context)['state'] == 'exception':
-            self.write(cr, uid, [ids], {'state':'except_send_mail','action_date':datetime.now()}, context)
-        else:
-            self.write(cr, uid, [ids], {'state':'refused','action_date':datetime.now()}, context)
-        return {'res_model':'purchase.order',
-                'view_mode':'form,tree',
-                'target':'current',
-                'res_id':attach.res_id,
-                'type':'ir.actions.act_window'
-                }
+        mail_status = False
+        state = 'refused'
+        if attach.send_mail_to_pay:
+            mail_status = 'sent'
+            template_id = self.pool.get("email.template").search(cr, uid, [('model_id','=','purchase.order'),('name','like','%Refus%')], context=context)
+            if isinstance(template_id, list):
+                template_id = template_id[0]
+            msg_id = self.pool.get("email.template").send_mail(cr, uid, template_id, attach.res_id, force_send=False, context=context)
+            self.pool.get("mail.message").write(cr, uid, [msg_id], {'attachment_ids':[(4, ids)]}, context=context)
+            self.pool.get("mail.message").send(cr, uid, [msg_id], context)
+            if self.pool.get("mail.message").read(cr, uid, msg_id, ['state'], context)['state'] == 'exception':
+                mail_status = 'failure'
+                #state = 'except_send_mail'
+        vals = {'send_mail_to_pay': False, 'state':state, 'action_date':datetime.now()}
+        if mail_status:
+            vals.update({'mail_to_pay_status': mail_status})
+        self.write(cr, uid, [ids], vals, context)
+        
+        return {'type':'ir.actions.act_window',
+        'res_model':'openstc.open.engage.refuse.inv.wizard',
+        'view_mode':'form',
+        'view_type':'form',
+        'target':'new',
+        'context':{'attach_id':ids}
+        }
     
     #Action du Boutton Permettant de Clore le suivi commande
     #Bloque s'il reste des factures à valider (état 'to_check' ou 'except_send_mail')
